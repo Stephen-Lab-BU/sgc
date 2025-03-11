c1f122c2d7c6e2292231651200ab47d855bcca72
John Tauber <john.tauber@gmail.com>
2025-03-11 15:00:02 -0400

    refactor dry

--- cohlib/confs/latent/simple.py
+++ cohlib/confs/latent/simple.py
@@ -3,7 +3,8 @@ from dataclasses import dataclass
 import jax.numpy as jnp
 import jax.random as jr
 
-from cohlib.jax.dists import LowRankCCN
+from cohlib.jax.dists import LowRankCCN, CCN
+from cohlib.jax.lr_model import rotate_eigvecs
 
 @dataclass
 class BasicSingleFreq:
@@ -15,43 +16,30 @@ class BasicSingleFreq:
     target_freq_ind: int = 9
     scale_power_target: float = 1.0e3
     L: int = 50
+    gamma_seed: int = 0
     seed: int = 7 
 
 @dataclass
-class BasicSingleFreqReLU:
+class BasicSingleFreqReLU(BasicSingleFreq):
     """
     Scaled version of BasicSingleFreq for point-process observations w/ ReLU Link
     """
     latent_type: str = 'basic_single_relu'
-    K: int = 3
-    rank: int = 1
-    num_freqs: int = 500 # assuming 1 second window 
-    # target_freq_inds: List[int] = field(default_factory=lambda: [9])
-    target_freq_ind: int = 9
     scale_power_target: float = 1.0e3*(3*1.0e4)
-    L: int = 50
-    seed: int = 0 
 
 @dataclass
-class BasicSingleFreqLog:
+class BasicSingleFreqLog(BasicSingleFreq):
     """
     Scaled version of BasicSingleFreq for point-process observations w/ Log Link
     """
     latent_type: str = 'basic_single_log'
-    K: int = 3
-    rank: int = 1
-    num_freqs: int = 500 # assuming 1 second window 
-    # target_freq_inds: List[int] = field(default_factory=lambda: [9])
-    target_freq_ind: int = 9
     scale_power_target: float = 1.0e6
-    L: int = 27
-    seed: int = 42 
 
-def create_lrccn_basic_rank1(lcfg):
+def create_lrccn_basic_rank1(lcfg, print_seed=True):
     K = lcfg.K
     N = lcfg.num_freqs
     target_freq_ind = lcfg.target_freq_ind
-    seed = lcfg.seed
+    gamma_seed = lcfg.gamma_seed
     freqs = jnp.arange(N)
     R = lcfg.rank
 
@@ -64,12 +52,15 @@ def create_lrccn_basic_rank1(lcfg):
     eigvecs_target = jnp.zeros((Nnz,K,R), dtype=complex)
     eigvals_target = jnp.zeros((Nnz,R))
 
-    lrk = jr.key(seed)
-    rksplit = jr.split(lrk, Nnz*R)
+    if print_seed:
+        print(f"Creating equal magnitude eigenvector with random phases (gamma seed: {gamma_seed})")
+
+    gamma_rk = jr.key(gamma_seed)
+    rksplit = jr.split(gamma_rk, Nnz*R)
     for j in range(Nnz):
         for r in range(R):
             if Nnz == 1 and R == 1:
-                rk = lrk
+                rk = gamma_rk
             else:
                 rk = rksplit[j+r]
             phases = jr.uniform(rk, (K,), minval=-jnp.pi, maxval=jnp.pi)
@@ -87,3 +78,41 @@ def create_lrccn_basic_rank1(lcfg):
 
     lrccn = LowRankCCN(eigvals_target, eigvecs_target, K, freqs, nz)
     return lrccn
+
+def create_ccn_basic_fullrank(lcfg, print_seed=True):
+
+    assert lcfg.K == lcfg.rank
+    K = lcfg.K
+    gamma_seed = lcfg.gamma_seed
+    N = lcfg.num_freqs
+    target_freq_ind = lcfg.target_freq_ind
+    gamma_seed = lcfg.gamma_seed
+
+    nz = jnp.array([target_freq_ind])
+
+    freqs = jnp.arange(N)
+    gamma_seed = 3
+    Nnz = 1
+
+    if print_seed:
+        print(f"Creating equal magnitude eigenvector with random phases (gamma seed: {gamma_seed})")
+
+    gamma = jnp.zeros((Nnz, K, K), dtype=complex)
+    gamma_rk = jr.key(gamma_seed)
+    rksplit = jr.split(gamma_rk, Nnz)
+    for j in range(Nnz):
+        if Nnz == 1: 
+            rk = gamma_rk
+        else:
+            rk = rksplit[j]
+
+        rksplit2 = jr.split(rk, 2)
+        Q, _ = jnp.linalg.qr(jr.normal(rksplit2[0], (K,K)) + jr.normal(rksplit2[1], (K,K))*1j)
+
+        Q = rotate_eigvecs(Q[None,:,:])
+
+        gamma_j = Q[j,:,:] @ jnp.diag(jnp.array([1e6, 1e3, 1e2])*K) @ Q[j,:,:].conj().T
+        gamma = gamma.at[j,:,:].set(gamma_j)
+
+    ccn = CCN(gamma, freqs, nz)
+    return ccn
