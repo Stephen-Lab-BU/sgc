79e928df2bb784a5d9c86d62d10594b300ce849f
John Tauber <john.tauber@gmail.com>
2025-03-14 15:13:06 -0400

    move latent funcs to own file; deprecate dists.py

--- cohlib/jax/latent.py
+++ cohlib/jax/latent.py
@@ -1,126 +1,7 @@
 import jax.numpy as jnp
 import jax.random as jr
 
-# TODO split up latent and observations into separate files
-# Latent
-# TODO 
-# generalize
-# - create generic 'ccn_sample' function and use
-
-# Generics
-# TODO add references for why this works
-def sample_ccn(rk, cov, L):
-    """
-    Generate L samples from K-dimensional multivariate complex 
-    normal (circular symmetric). 'cov' must be psd Hermitian.
-
-    Args:
-        rk: jax random key
-        cov: (K,K) array PSD Hermitian covariance matrix
-        L: number of samples
-
-    Returns:
-        samples: (K,L) array (complex)
-    
-    """
-    assert jnp.all(jnp.isclose(cov.conj().T, cov, atol=1e-9))
-    K = cov.shape[0]
-
-    eigvals, eigvecs = jnp.linalg.eigh(cov)
-    # assert jnp.all(eigvals >= 0)
-
-    D = jnp.diag(jnp.sqrt(eigvals))
-    A = eigvecs @ D
-
-    unit_samples = jr.normal(rk, (K,L), dtype=complex)
-    samples = jnp.einsum('ki,il->kl', A, unit_samples)
-
-    return samples
-
-def sample_lrccn(rk, eigvecs_lr, eigvals_lr, K, L):
-    R = eigvals_lr.size
-
-    eigvecs = jnp.zeros((K,K), dtype=complex)
-    eigvecs = eigvecs.at[:,:R].set(eigvecs_lr)
-
-    eigvals = jnp.zeros(K)
-    eigvals = eigvals.at[:R].set(eigvals_lr)
-
-    D = jnp.diag(jnp.sqrt(eigvals))
-    A = eigvecs @ D
-
-    unit_samples = jr.normal(rk, (K,L), dtype=complex)
-    samples = jnp.einsum('ki,il->kl', A, unit_samples)
-
-    return samples
-
-
-# TODO deprecate
-def sample_ccn_rank1(rk, eigvec, eigval, K, L):
-    """
-    Generate L samples from K-dimensional multivariate complex 
-    normal (circular symmetric). 'cov' must be psd Hermitian.
-
-    Args:
-        rk: jax random key
-        cov: (K,K) array PSD Hermitian covariance matrix
-        L: number of samples
-
-    Returns:
-        samples: (K,L) array (complex)
-    
-    """
-    eigvecs = jnp.zeros((K,K), dtype=complex)
-    eigvecs = eigvecs.at[:,0].set(eigvec)
-
-    eigvals = jnp.zeros(K)
-    eigvals = eigvals.at[0].set(eigval)
-
-    D = jnp.diag(jnp.sqrt(eigvals))
-    A = eigvecs @ D
-
-    unit_samples = jr.normal(rk, (K,L), dtype=complex)
-    samples = jnp.einsum('ki,il->kl', A, unit_samples)
-
-    return samples
-
-
-# Application
-# deprecated
-def DEPsample_from_gamma(rk, gamma, L):
-    """
-    Generate L samples from multivariate complex normal (circular symmetric). 
-    Gamma is assumed to be indepedent across frequencies (block-diagonal).
-
-    Args:
-        gamma: (N,K,K) array where N is frequencies in nyquist 
-            range, K is number of units
-        L: number of samples to generate
-        rk: jax random key
-    Return:
-        samples: (N,K) array (complex)
-    """
-    N = gamma.shape[0]
-
-    rksplit = jr.split(rk,N)
-    samples = jnp.stack([sample_ccn(rksplit[n], gamma[n,:,:], L) for n in range(N)])
-    # samples = jnp.stack([sample_ccn(rk+n, gamma[n,:,:], L) for n in range(N)])
-
-    return samples
-
-
-def naive_estimator(spikes, nonzero_inds=None):
-    "spikes has shape (time, unit, trial)"
-    n_f0 = jnp.fft.rfft(spikes, axis=0)
-    n_f = n_f0[1:,:,:]
-    naive_est = jnp.einsum('jkl,jil->jkil', n_f, n_f.conj()).mean(-1)
-
-    if nonzero_inds is None:
-        return naive_est
-    else:
-        return naive_est[nonzero_inds, :, :]
-
-
+# TODO add abc
 class LowRankCCN():
     def __init__(self, eigvals, eigvecs, dim, freqs, nonzero_inds):
         self.freqs = freqs
@@ -227,3 +108,48 @@ class CCN():
         samples = samples.at[self.nz,:,:].set(samples_nz)
         return samples
 
+# TODO add references for why this works
+def sample_ccn(rk, cov, L):
+    """
+    Generate L samples from K-dimensional multivariate complex 
+    normal (circular symmetric). 'cov' must be psd Hermitian.
+
+    Args:
+        rk: jax random key
+        cov: (K,K) array PSD Hermitian covariance matrix
+        L: number of samples
+
+    Returns:
+        samples: (K,L) array (complex)
+    
+    """
+    assert jnp.all(jnp.isclose(cov.conj().T, cov, atol=1e-9))
+    K = cov.shape[0]
+
+    eigvals, eigvecs = jnp.linalg.eigh(cov)
+    # assert jnp.all(eigvals >= 0)
+
+    D = jnp.diag(jnp.sqrt(eigvals))
+    A = eigvecs @ D
+
+    unit_samples = jr.normal(rk, (K,L), dtype=complex)
+    samples = jnp.einsum('ki,il->kl', A, unit_samples)
+
+    return samples
+
+def sample_lrccn(rk, eigvecs_lr, eigvals_lr, K, L):
+    R = eigvals_lr.size
+
+    eigvecs = jnp.zeros((K,K), dtype=complex)
+    eigvecs = eigvecs.at[:,:R].set(eigvecs_lr)
+
+    eigvals = jnp.zeros(K)
+    eigvals = eigvals.at[:R].set(eigvals_lr)
+
+    D = jnp.diag(jnp.sqrt(eigvals))
+    A = eigvecs @ D
+
+    unit_samples = jr.normal(rk, (K,L), dtype=complex)
+    samples = jnp.einsum('ki,il->kl', A, unit_samples)
+
+    return samples
